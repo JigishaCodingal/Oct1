642fed15cac72d6f94063c5dab9ae4dcc9f95921
jigishacodingal <jigishacodingal@gmail.com>
2024-10-01 18:12:54 +0530

    hi

---
+++ p1.py
@@ -0,0 +1,15 @@
+val = int(input("Enter the value"))
+
+di = eval(input("Enter the dictionary"))
+
+for k,v in d.items():
+
+    if v == val:
+
+        f = f+1
+
+print("The value is on key",k)
+
+Enter value 13 :
+
+Enter dictionary:('a':12,'b':13,'c':14,)
